1b3b90416562edc38b14613c495012614d231d5b
404manveer <manveershah007@gmail.com>
2025-12-10 14:26:19 +0530

    home page

--- src/app/(frontend)/page.jsx
+++ src/app/(frontend)/page.jsx
@@ -1,3 +1,12 @@
+// export default async function profilePage() {
+ 
+//     return(
+//       <>
+//       <Innerbanner pagename={'Profile'} />
+//       <ProfileComponent id={user?.userId}/>
+//       </>
+//     )
+// }
 import Form from "@/components/formComponents/Form";
 // import Innerbanner from "@/components/sharedUi/Innerbanner"
 
