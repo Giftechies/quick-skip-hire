8f0c11c381cdfed8d269de120a5a51deafc23a74
404manveer <manveershah007@gmail.com>
2025-12-23 17:10:25 +0530

    fix

--- src/app/verify-otp/page.jsx
+++ src/app/verify-otp/page.jsx
@@ -4,8 +4,9 @@ import VerifyOtpPage from "./VerifyComponent";
 export default function Page() {
   return (
     <Suspense fallback={<div className="text-white">Loading...</div>}>
-      
+
       <VerifyOtpPage />
+      
     </Suspense>
   );
 }
